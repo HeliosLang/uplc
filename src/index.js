4b2b5f84886204230d78759a24ba85e236a4ebac
Christian Schmitz <cschmitz398@gmail.com>
2024-03-25 09:56:57 -0600

    exported decodeCost and encodeCost for CBOR

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ export * as v1 from "./v1/index.js"
 export * as v2 from "./v2/index.js"
 export * as v3 from "./v3/index.js"
 
+export { decodeCost, encodeCost } from "./v1/costmodel/index.js"
+
 /**
  * @typedef {import("./v1/costmodel/index.js").Cost} Cost
  */
--- src/v1/costmodel/Cost.js
+++ src/v1/costmodel/Cost.js
@@ -1,6 +1,37 @@
+import {
+    decodeInt,
+    decodeTuple,
+    encodeInt,
+    encodeTuple
+} from "@helios-lang/cbor"
+
+/**
+ * @typedef {import("@helios-lang/codec-utils").ByteArrayLike} ByteArrayLike
+ */
+
 /**
  * @typedef {{
  *   cpu: bigint
  *   mem: bigint
  * }} Cost
  */
+
+/**
+ * Decode Cost CBOR bytes
+ * @param {ByteArrayLike} bytes
+ * @returns {Cost}
+ */
+export function decodeCost(bytes) {
+    const [mem, cpu] = decodeTuple(bytes, [decodeInt, decodeInt])
+
+    return { cpu, mem }
+}
+
+/**
+ * Encode Cost using CBOR
+ * @param {Cost} cost
+ * @returns {number[]}
+ */
+export function encodeCost(cost) {
+    return encodeTuple([encodeInt(cost.mem), encodeInt(cost.cpu)])
+}
