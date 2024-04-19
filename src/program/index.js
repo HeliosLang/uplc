40cc8cd68674eae91b7c39a7fed71ab439b7501a
Christian Schmitz <cschmitz398@gmail.com>
2024-04-19 11:42:30 -0600

    exported PlutusVersion

--- src/program/index.js
+++ src/program/index.js
@@ -2,5 +2,6 @@ export { UplcProgramV1 } from "./UplcProgramV1.js"
 export { UplcProgramV2 } from "./UplcProgramV2.js"
 
 /**
+ * @typedef {import("./UplcProgram.js").PlutusVersion} PlutusVersion
  * @typedef {import("./UplcProgram.js").UplcProgram} UplcProgram
  */
