cac74bcccb3426ad2738f179f5d601d42ff6f8ce
christianschmitz <cschmitz398@gmail.com>
2025-05-14 13:39:02 -0600

    decodeTerm uses a stack based algorithm instead of a recursive algorithm

--- src/terms/decode.js
+++ src/terms/decode.js
@@ -1,10 +1,11 @@
+import { expectDefined } from "@helios-lang/type-utils"
 import { UPLC_BUILTIN_TAG, decodeUplcBuiltinFromFlat } from "./UplcBuiltin.js"
-import { UPLC_CALL_TAG, decodeUplcCallFromFlat } from "./UplcCall.js"
+import { UPLC_CALL_TAG, makeUplcCall } from "./UplcCall.js"
 import { UPLC_CONST_TAG, decodeUplcConstFromFlat } from "./UplcConst.js"
-import { UPLC_DELAY_TAG, decodeUplcDelayFromFlat } from "./UplcDelay.js"
+import { UPLC_DELAY_TAG, makeUplcDelay } from "./UplcDelay.js"
 import { UPLC_ERROR_TAG, makeUplcError } from "./UplcError.js"
-import { UPLC_FORCE_TAG, decodeUplcForceFromFlat } from "./UplcForce.js"
-import { UPLC_LAMBDA_TAG, decodeUplcLambdaFromFlat } from "./UplcLambda.js"
+import { UPLC_FORCE_TAG, makeUplcForce } from "./UplcForce.js"
+import { UPLC_LAMBDA_TAG, makeUplcLambda } from "./UplcLambda.js"
 import { UPLC_VAR_TAG, decodeUplcVarFromFlat } from "./UplcVar.js"
 
 /**
@@ -12,32 +13,90 @@ import { UPLC_VAR_TAG, decodeUplcVarFromFlat } from "./UplcVar.js"
  */
 
 /**
- * Reads a single UplcTerm
+ * Reads a single UplcTerm using a stack-based algorithm
  * @param {FlatReader} r
  * @param {Builtin[]} builtins
  * @returns {UplcTerm}
  */
 export function decodeTerm(r, builtins) {
-    const tag = r.readTag()
+    /**
+     * Undefined -> decoding
+     * Defined -> collecting
+     * @type {UplcTerm | undefined}
+     */
+    let term = undefined
 
-    switch (tag) {
-        case UPLC_VAR_TAG:
-            return decodeUplcVarFromFlat(r)
-        case UPLC_DELAY_TAG:
-            return decodeUplcDelayFromFlat(r)
-        case UPLC_LAMBDA_TAG:
-            return decodeUplcLambdaFromFlat(r)
-        case UPLC_CALL_TAG:
-            return decodeUplcCallFromFlat(r) // aka function application
-        case UPLC_CONST_TAG:
-            return decodeUplcConstFromFlat(r)
-        case UPLC_FORCE_TAG:
-            return decodeUplcForceFromFlat(r)
-        case UPLC_ERROR_TAG:
-            return makeUplcError()
-        case UPLC_BUILTIN_TAG:
-            return decodeUplcBuiltinFromFlat(r, builtins)
-        default:
-            throw new Error("term tag " + tag.toString() + " unhandled")
+    /**
+     * @type {({
+     *   kind: "delay"
+     * } | {
+     *   kind: "lambda"
+     * } | {
+     *   kind: "applyfn"
+     * } | {
+     *   kind: "force"
+     * } | {
+     *   kind: "apply"
+     *   fn: UplcTerm
+     * })[]}
+     */
+    const collect = []
+
+    while (!term || collect.length > 0) {
+        if (!term) {
+            const tag = r.readTag()
+
+            switch (tag) {
+                case UPLC_VAR_TAG:
+                    term = decodeUplcVarFromFlat(r)
+                    break
+                case UPLC_DELAY_TAG:
+                    collect.push({ kind: "delay" })
+                    break
+                case UPLC_LAMBDA_TAG:
+                    collect.push({ kind: "lambda" })
+                    break
+                case UPLC_CALL_TAG:
+                    collect.push({ kind: "applyfn" })
+                    break
+                case UPLC_CONST_TAG:
+                    term = decodeUplcConstFromFlat(r)
+                    break
+                case UPLC_FORCE_TAG:
+                    collect.push({ kind: "force" })
+                    break
+                case UPLC_ERROR_TAG:
+                    term = makeUplcError()
+                    break
+                case UPLC_BUILTIN_TAG:
+                    term = decodeUplcBuiltinFromFlat(r, builtins)
+                    break
+                default:
+                    throw new Error("term tag " + tag.toString() + " unhandled")
+            }
+        } else {
+            const c = expectDefined(collect.pop())
+
+            switch (c.kind) {
+                case "apply":
+                    term = makeUplcCall({ fn: c.fn, arg: term })
+                    break
+                case "applyfn":
+                    collect.push({ kind: "apply", fn: term })
+                    term = undefined
+                    break
+                case "delay":
+                    term = makeUplcDelay({ arg: term })
+                    break
+                case "force":
+                    term = makeUplcForce({ arg: term })
+                    break
+                case "lambda":
+                    term = makeUplcLambda({ body: term })
+                    break
+            }
+        }
     }
+
+    return expectDefined(term, "term decoding failed")
 }
